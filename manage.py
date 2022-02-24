141db11ae183bdfe3329dfefa918bd9fa8c1a56e
hya14217 <cyang.bupt@outlook.com>
2022-02-24 22:38:21 +0800

    auto commit

---
+++ manage.py
@@ -0,0 +1,64 @@
+import os
+import json
+import webbrowser
+from gevent import pywsgi as wsgi
+from pathlib import Path
+from flask import Flask, request
+from preprocess import preprocess
+from hashlib import md5
+
+
+HOST = "192.168.0.104"
+PORT = 8888
+
+def web_app():
+    app = Flask(__name__, static_folder="static", static_url_path="/")
+
+    def get_file_index():
+        indices = []
+        for img_file in Path("assets/upload/").glob("*.jpg"):
+            idx = int(img_file.name.split(".")[0])
+            indices.append(idx)
+        indices.sort()
+        prev_i = 0
+        for i in indices:
+            if i - prev_i > 1:
+                return prev_i + 1
+            prev_i = i
+        return prev_i + 1
+
+    @app.route("/upload", methods=["POST"])
+    def upload():
+        file = request.files["file"]
+        content = file.stream.read()
+        suffix = file.filename.split(".")[-1]
+        filename = md5(content).hexdigest() + "." + suffix
+        path = os.path.join("static/assets/upload", filename)
+        with open(path, "wb") as fout:
+            fout.write(content)
+        preprocess(Path(path))
+        return filename
+    
+    @app.route("/sync_photo_data", methods=["POST"])
+    def sync_photo_data():
+        photos = request.form["photos"]
+        with open("data.js", "w", encoding="utf-8") as fout:
+            fout.write("var photos=" + photos)
+        os.system("commit.cmd")
+        return photos
+    
+    @app.route("/")
+    def index():
+        with open("templates/index.html", "r", encoding="utf-8") as fin:
+            return fin.read()
+
+    webbrowser.open(f"http://{HOST}:{PORT}")
+    server = wsgi.WSGIServer((HOST, PORT), app)
+    server.serve_forever()
+    return app
+
+
+if __name__ == "__main__":
+    app = web_app()
+    server = wsgi.WSGIServer((HOST, PORT), app)
+    server.serve_forever()
--- preprocess.py
+++ preprocess.py
@@ -14,33 +14,34 @@ PREVIEW_SIZE = 1920
 
 def process_datetime(timestamp):
     dt = datetime.fromtimestamp(timestamp)
-    return f"{dt.year}/{dt.month}/{dt.day} {dt.hour}:{dt.minute}"
+    return f"{dt.year}/{dt.month}/{dt.day} {dt.hour:2}:{dt.minute:2}:{dt.minute:2}"
+
+
+def preprocess(img_file):
+    # 压缩
+    quality = ZIP_RATIO
+    o_size = os.path.getsize(img_file) / 1024
+    thumbnail_outfile = os.path.join(img_file.parent.parent, "thumbnail", img_file.name)
+    img_outfile = os.path.join(img_file.parent.parent, "img", img_file.name)
+    if o_size <= ZIP_THRESHOLD:
+        im = Image.open(img_file)
+        im.save(thumbnail_outfile)
+        im.save(img_outfile)
+        return
+    while o_size > ZIP_THRESHOLD:
+        im = Image.open(img_file)
+        im.resize((PREVIEW_SIZE, im.size[1]*PREVIEW_SIZE//im.size[0])).save(img_outfile, quality=quality)
+        im.resize((THUMBNAIL_SIZE, im.size[1]*THUMBNAIL_SIZE//im.size[0])).save(thumbnail_outfile, quality=quality)
+        if quality - QUALITY_STEP < 0:
+            return
+        quality -= QUALITY_STEP
+        o_size = os.path.getsize(thumbnail_outfile) / 1024
+
+
+def main():
+    for img_file in Path("assets/upload").glob("*.jpg"):
+        preprocess(img_file)
 
 
 if __name__ == "__main__":
-
-    dates = []
-    for img_file in Path("assets/upload").glob("*.jpg"):
-        mtime = os.path.getmtime(img_file)
-        dates.append(process_datetime(mtime))
-        # 压缩
-        quality = ZIP_RATIO
-        o_size = os.path.getsize(img_file) / 1024
-        thumbnail_outfile = os.path.join(img_file.parent.parent, "thumbnail", img_file.name)
-        img_outfile = os.path.join(img_file.parent.parent, "img", img_file.name)
-        if o_size <= ZIP_THRESHOLD:
-            im = Image.open(img_file)
-            im.save(thumbnail_outfile)
-            im.save(img_outfile)
-            continue
-        while o_size > ZIP_THRESHOLD:
-            im = Image.open(img_file)
-            im.resize((PREVIEW_SIZE, im.size[1]*PREVIEW_SIZE//im.size[0])).save(img_outfile, quality=quality)
-            im.resize((THUMBNAIL_SIZE, im.size[1]*THUMBNAIL_SIZE//im.size[0])).save(thumbnail_outfile, quality=quality)
-            if quality - QUALITY_STEP < 0:
-                break
-            quality -= QUALITY_STEP
-            o_size = os.path.getsize(thumbnail_outfile) / 1024
-
-    with open("dates.js", "w", encoding="utf-8") as fout:
-        fout.write(f"var dates = {str(dates)}")
+    main()
