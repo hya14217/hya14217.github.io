4c3abb6b97b1bbb368612cede9dfe1c657de544e
hya14217 <cyang.bupt@outlook.com>
2022-02-24 13:54:28 +0800

    more photos

--- preprocess.py
+++ preprocess.py
@@ -11,11 +11,15 @@ def process_datetime(timestamp):
     return f"{dt.year}/{dt.month}/{dt.day} {dt.hour}:{dt.minute}"
 
 for img_file in Path("assets/upload").glob("*.jpg"):
+    mtime = os.path.getmtime(img_file)
+    dates.append(process_datetime(mtime))
     # 压缩
     o_size = os.path.getsize(img_file) / 1024
-    if o_size <= 500:
-        continue
     outfile = os.path.join(img_file.parent.parent, "img", img_file.name)
+    if o_size <= 500:
+        im = Image.open(img_file)
+        im.save(outfile, quality=80)
+        continue
     quality = 80
     while o_size > 500:
         im = Image.open(img_file)
@@ -24,8 +28,6 @@ for img_file in Path("assets/upload").glob("*.jpg"):
             break
         quality -= 10
         o_size = os.path.getsize(outfile) / 1024
-    mtime = os.path.getmtime(img_file)
-    dates.append(process_datetime(mtime))
 
 with open("dates.js", "w", encoding="utf-8") as fout:
     fout.write(f"var dates = {str(dates)}")
